3223249fec20da253e244375dabe11f2b8d90e9a
mjockers <mjockers@gmail.com>
2015-02-02 07:43:40 -0600

    Update Vignette

--- inst/doc/syuzhet-vignette.R
+++ inst/doc/syuzhet-vignette.R
@@ -2,7 +2,7 @@
 library(syuzhet)
 my_example_text <- "I begin this story with a neutral statement.  
   Basically this is a very silly test.  
-  You are testing the syuchet package using short, inane sentences.  
+  You are testing the Syuzhet package using short, inane sentences.  
   I am actually very happy today. 
   I have finally finished writing this package.  
   Tomorrow I will be very sad. 
