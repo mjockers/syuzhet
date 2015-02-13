26d06938d775e9b5a75e8f9db41b45d03e311952
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-02-13 10:19:07 -0500

    Tests for getting sentences

--- tests/testthat/test-reading.R
+++ tests/testthat/test-reading.R
@@ -7,3 +7,13 @@ test_that("Text files can be read locally and parsed correctly",{
   expect_that(joyce, is_a("String"))
   expect_that(length(joyce), equals(1))
 })
+
+context("Parsing sentences")
+
+sents <- get_sentences(joyce)
+
+test_that("Sentences are correctly parsed.", {
+  expect_that(sents, is_a("character"))
+  expect_true(length(sents) > 1 ) # i.e., it's not just a single string anymore
+  expect_equal(sents[2], "He was baby tuckoo.")
+})
