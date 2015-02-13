88bfa95252b8867df1bf145b07e62aab2ad292d9
Lincoln Mullen <lincoln@lincolnmullen.com>
2015-02-13 10:26:51 -0500

    Tests for getting sentiment

--- tests/testthat/test-reading.R
+++ tests/testthat/test-reading.R
@@ -17,3 +17,18 @@ test_that("Sentences are correctly parsed.", {
   expect_true(length(sents) > 1 ) # i.e., it's not just a single string anymore
   expect_equal(sents[2], "He was baby tuckoo.")
 })
+
+context("Getting sentiment")
+
+bing  <- get_sentiment(sents, method = "bing")
+afinn <- get_sentiment(sents, method = "afinn")
+nrc   <- get_sentiment(sents, method = "nrc")
+
+test_that("Sentiments are returned correctly", {
+  expect_that(bing, is_a("integer"))
+  expect_that(afinn, is_a("integer"))
+  expect_that(nrc, is_a("numeric")) # because it is averages
+  expect_equal(length(sents), length(bing))
+  expect_equal(length(sents), length(afinn))
+  expect_equal(length(sents), length(nrc))
+})
